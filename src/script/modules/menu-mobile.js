2241d93be2d5894ad5d9bfa2ce0fa6af0960283a
Gustavo Monteiro <gu_cm@hotmail.com>
2022-07-03 02:42:27 -0300

    adicionado efeito de scroll, e outras correções

--- src/script/modules/menu-mobile.js
+++ src/script/modules/menu-mobile.js
@@ -1,24 +1,24 @@
-export default function menuMobile() {}
+export default function menuMobile() {
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
 
-const menuButton = document.querySelector('[data-menu="button"]');
-const menuList = document.querySelector('[data-menu="list"]');
-
-function handleClick(event) {
-  event.preventDefault();
-  menuList.classList.toggle("hidden");
-  menuButton.classList.toggle("hidden");
-  menuButton.classList.toggle("visible");
-  menuButton.classList.toggle("fullbutton");
-}
-
-function closeMenu(event) {
-  if (event.target !== menuList && event.target !== menuButton) {
-    menuList.classList.add("hidden");
-    menuButton.classList.add("hidden");
-    menuButton.classList.remove("visible");
-    menuButton.classList.add("fullbutton");
+  function handleClick(event) {
+    event.preventDefault();
+    menuList.classList.toggle("hidden");
+    menuButton.classList.toggle("hidden");
+    menuButton.classList.toggle("visible");
+    menuButton.classList.toggle("fullbutton");
   }
-}
 
-menuButton.addEventListener("click", handleClick);
-window.addEventListener("click", closeMenu);
+  function closeMenu(event) {
+    if (event.target !== menuList && event.target !== menuButton) {
+      menuList.classList.add("hidden");
+      menuButton.classList.add("hidden");
+      menuButton.classList.remove("visible");
+      menuButton.classList.add("fullbutton");
+    }
+  }
+
+  menuButton.addEventListener("click", handleClick);
+  window.addEventListener("click", closeMenu);
+}
